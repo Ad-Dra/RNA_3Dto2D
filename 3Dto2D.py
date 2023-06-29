cd7e270ae6a849df86cb6d44626befe94781f892
Andrea-Lolli <lolliandrea17@gmail.com>
2023-06-29 16:57:17 +0200

    add new files

---
+++ 3Dto2D.py
@@ -0,0 +1,335 @@
+import argparse
+import os
+import sys
+
+from FR3D.fr3d.classifiers.NA_pairwise_interactions import annotate_nt_nt_in_structure, focus_basepair_cutoffs, load_structure, myTimer, simplify_basepair
+from FR3D.fr3d.classifiers.hydrogen_bonds import load_ideal_basepair_hydrogen_bonds
+from FR3D.fr3d.classifiers.class_limits_2023 import nt_nt_cutoffs
+from RNASecodaryStructure import RNASecondaryStructure   # use latest cutoffs
+
+#convert the input string so that the first letter is to lower case andd second and third letter are to upper case
+def adapt_category_format(string):
+    if len(string) != 3:
+        print("THE CATEGORY: " + string + " IS NOT RECOGNISED!")
+        sys.exit()
+    
+    first_letter = string[0].lower()
+    second_letter = string[1].upper()
+    third_letter = string[2].upper()
+    
+    return first_letter + second_letter + third_letter
+
+#used to compute sequence
+def extract_triplets(input_string):
+
+    start_index = input_string.find("'p_1': [") #TODO NON SONO SICURO CHE QUESTA SEZIONE VIENE SEMPRE CHIAMATA 'p_1' DA CONTROLLAREE!!!!!!!!! 
+    end_index = input_string.find("]", start_index)
+    section_string = input_string[start_index:end_index+1]
+
+    triplets = []
+    objects = section_string.split("), (")
+    for obj in objects:
+        parts = obj.split("|")
+        if len(parts) >= 4:
+
+            number = parts[4]; 
+            index = number.find("'")
+            if index != -1:
+                number = number[:index + len("'")]
+
+            number1 = parts[8]
+            index = number1.find("'")
+            if index != -1:
+                number1 = number1[:index + len("'")]
+            
+            triplet = [parts[2], parts[3], number.replace("'","")]
+            triplet1 = [parts[6], parts[7], number1.replace("'","")]
+            
+            if triplet not in triplets:
+                triplets.append(triplet)
+            if triplet1 not in triplets:
+                triplets.append(triplet1)
+
+    return triplets
+
+#returns the pairs ['number', 'nucleobase'] of each nucleobase in the chain
+def compute_sequence(chain):
+    sequence = []
+
+    for base in chain:
+        pair = []
+        pair.append(base[2])
+        if (len(base[1]) > 1):
+            pair.append(base[1][len(base[1]) - 1])
+        else:
+            pair.append(base[1])
+        sequence.append(pair)
+
+    return sequence
+
+#returns an array containing every model found as ints
+def modelFound(input_string):
+    start_index = input_string.find("'p_1': [") #Forse p_1 cambia qualche volta?
+    end_index = input_string.find("]", start_index)
+    section_string = input_string[start_index:end_index+1]
+
+    result = []
+    objects = section_string.split("), (")
+    for obj in objects:
+        parts = obj.split("|")
+        if len(parts) >= 4: #controlla che non è None
+            model = parts[1]; 
+            if model not in result:
+                result.append(model)
+
+    return result
+
+#returns true if the model number corresponds to the input
+def isSameModelNumber(a, model):
+    n = a.split("|")
+    if str(n[1]) == str(model):
+        return True
+    else: 
+        return False
+
+def extractSecondaryStrincture(pdbid,interaction_to_list_of_tuples,categories,category_to_interactions, cat, modelNumber):
+
+    sequence = compute_sequence(extract_triplets(str(interaction_to_list_of_tuples))) 
+    rna_structure = RNASecondaryStructure(sequence, pdbid)
+
+    for category in categories.keys():
+        
+        for interaction in category_to_interactions[category]:
+            inter = interaction
+            if category == 'basepair':
+                inter = simplify_basepair(interaction)
+            
+            if len(categories[category]) == 0 or inter in categories[category]:
+                for a,b,c in interaction_to_list_of_tuples[interaction]:
+                    if inter in cat:
+                        if isSameModelNumber(a, abs(int(modelNumber))):
+                            b1 = int((a.split("|"))[4])
+                            b2 = int((b.split("|"))[4])
+                            rna_structure.add_bond(b1, b2, str(inter), modelNumber)                
+        
+        return rna_structure
+
+# Remove bpseq format from output list and replaces it with aas format
+def adapt_multibond_output(output_formats): 
+    res = []
+
+    for optf in output_formats:
+        if optf == "bpseq":
+            if "aas" not in output_formats:
+                res.append("aas")
+        else:
+            res.append(optf)
+
+    return res
+
+
+def write_txt_output_file(outputFile,pdbid,interaction_to_list_of_tuples,categories,category_to_interactions, cat, opt, allStructure, allAnnotations, mn):
+    """
+    Write interactions according to category, and within each
+    category, write by annotation.
+    Other than that, the interactions are listed in no particular order.
+    """
+
+    models = modelFound(str(interaction_to_list_of_tuples))
+
+    if (mn == []):
+        mn = models
+
+    for model in mn:
+        if model not in models:
+            return
+        if len(models) == 1:
+            model = -1
+
+        secondary_structure = extractSecondaryStrincture(pdbid,interaction_to_list_of_tuples,categories,category_to_interactions, cat, model)
+
+        if(not allStructure and not allAnnotations): # Se l'utente non specifica -a ne -aa (1 file per ogni legame)
+            for c in cat:   
+                secondary_structure.write_output_file([c], model, opt, outputFile)
+
+        elif(allAnnotations): # Se l'utente scrive -aa (tutti i legami sullo stesso file + 1 file per ogni legame)
+            secondary_structure.write_output_file(cat, model, adapt_multibond_output(opt), outputFile)
+            for c in cat:
+                secondary_structure.write_output_file([c], model, opt, outputFile)
+        
+        elif(allStructure): # Se l'utente scrive -a (tutti i legami sullo stesso file)
+            secondary_structure.write_output_file(cat, model, adapt_multibond_output(opt), outputFile)                
+
+#OKKK
+def generate_output_files(entry_id, inputPath, outputPath, category, opt, allStructure, allAnnotations, mn):
+
+    if isinstance(entry_id,str):
+        entry_id = [entry_id]
+
+    # dictionary to control what specific annotations are output, in a file named for the key
+    # empty list means to output all interactions in that category
+    # non-empty list specifies which interactions to output in that category
+    categories = {}
+
+    Leontis_Westhof_basepairs = ['cWW', 'cSS', 'cHH', 'cHS', 'cHW', 'cSH', 'cSW', 'cWH', 'cWS', 'tSS', 'tHH', 'tHS', 'tHW', 'tSH', 'tSW', 'tWH', 'tWS', 'tWW']
+    
+    cat = Leontis_Westhof_basepairs
+    
+    if category != 'basepair':
+        cat = category.split(",")
+        for i in range(len(cat)):
+            cat[i] = adapt_category_format(cat[i]) #adapt upper and lower cases
+            if cat[i] not in Leontis_Westhof_basepairs:
+                print("THE CATEGORY: " + cat[i] + " IS NOT RECOGNISED!")
+                sys.exit()
+
+        print(str(cat))
+
+    if category:
+        for category in category.split(","):
+            categories[category] = []
+    else:
+        # default is to annotate and write just "true" basepairs
+        categories['basepair'] = Leontis_Westhof_basepairs
+
+    # only output true basepairs in these main categories
+    if 'basepair' in categories:
+        categories['basepair'] = Leontis_Westhof_basepairs
+    else:
+        categories['basepair'] = cat #caso in cui l'utente specifica le categorie
+
+    # check existence of input path
+    if len(inputPath) > 0 and not os.path.exists(inputPath):
+        print("Attempting to create input path %s" % inputPath)
+        os.mkdir(inputPath)
+
+    # check existence of output path
+    if len(outputPath) > 0 and not os.path.exists(outputPath):
+        print("Attempting to create output path %s" % outputPath)
+        os.mkdir(outputPath)
+
+    # process additional arguments as PDB files
+    PDBs = []  # list of (path,filename) entries
+    entries = entry_id
+    for entry in entries:
+        # identify path to the PDB file, if any
+        path_split = os.path.split(entry)   # produces a tuple ACGUUACCAA
+
+        if len(path_split[0]) > 0:
+            PDBs.append(path_split)
+        else:
+            PDBs.append((inputPath,entry))
+
+    # annotate each PDB file
+    timerData = myTimer("start")
+    failed_structures = []
+    counter = 0
+
+    # restrict dictionary of cutoffs to just the basepairs needed here
+    focused_basepair_cutoffs = focus_basepair_cutoffs(nt_nt_cutoffs,categories['basepair'])
+    ideal_hydrogen_bonds = load_ideal_basepair_hydrogen_bonds()
+
+    """
+    for combination in ideal_hydrogen_bonds:
+        for LW in ideal_hydrogen_bonds[combination]:
+            print(combination, LW, ideal_hydrogen_bonds[combination][LW])
+    """
+
+    for path, PDB in PDBs:
+        counter += 1
+
+        # attempt to identify the main file identifier, could be a 4-character pdb id
+        pdbid = PDB.replace(".cif","").replace(".pdb","").replace(".gz","")
+
+        filename = os.path.join(path,PDB)
+
+        print("Reading file %s, which is number %d out of %d" % (filename, counter, len(PDBs)))
+        timerData = myTimer("Reading CIF files",timerData)
+
+        # suppress error messages, but report failures at the end
+        structure, messages = load_structure(filename,pdbid)
+
+        if not structure:
+            for message in messages:
+                failed_structures.append((pdbid,message))
+            continue
+
+        interaction_to_list_of_tuples, category_to_interactions, timerData, pair_to_data = annotate_nt_nt_in_structure(structure,categories,focused_basepair_cutoffs,ideal_hydrogen_bonds,[],timerData)
+        timerData = myTimer("Recording interactions",timerData)
+        print("  Recording interactions in %s" % outputPath)
+
+        #write txt output
+        write_txt_output_file(outputPath,pdbid,interaction_to_list_of_tuples,categories,category_to_interactions, cat, opt, allStructure, allAnnotations, mn)
+
+    myTimer("summary",timerData)
+
+    if len(failed_structures) > 0:
+        print("Error messages:")
+        for message in failed_structures:
+            print("%s %s" % message)
+    else:
+        print("All files read successfully")
+
+    # note status of stacking annotations
+    if 'stacking' in categories:
+        print("Stacking annotations are not yet finalized")
+
+    if 'basepair' in categories:
+        print("Basepair annotations are not yet finalized")
+
+if __name__=="__main__":
+
+    # allow user to specify input and output paths
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input_path', help='input paths of the files containing the 3d structure')
+    parser.add_argument('output_FolderPath', help='output path of the folder in which the program will produce the output')
+    # aggiunte:
+    parser.add_argument('-l', "--category", help='Interaction category or categories for the output separated by comma (cWW,tHS,tSW...)')
+    parser.add_argument('-o', "--outputFormat", help="Establish the type of output format (aas,bpseq,tkz,png)")
+    parser.add_argument('-a',  action='store_true', help="Annotates every bond type in one output file (the format can only be aas!)")
+    parser.add_argument('-aa',  action='store_true', help="Generates one output file for each bond type and a file with every bond in it (the output format can only be aas!)")
+    parser.add_argument('-mn', "--modelNumber", help="Writes only output in model number specified (mn1,mn2,mn3,mn4,...)")
+
+    problem = False
+    args = parser.parse_args()
+
+    output_FolderPath = args.output_FolderPath
+
+    input_Path = args.input_path
+
+    if args.category:
+        category = args.category
+    else: 
+        category = 'basepair'
+
+    if args.modelNumber:
+        mn = args.modelNumber.split(",")
+    else:
+        mn = []
+
+    if args.outputFormat:
+        opt = [item.lower() for item in args.outputFormat.split(",")]
+    else:
+        opt = ["aas"] #default = aas
+
+    for outputType in opt:
+        if(outputType != "aas" and outputType != "bpseq" and outputType != "tkz" and outputType != "png"):
+            print ("OUTPUT FORMAT IS NOT VALID: " , outputType)
+            sys.exit()
+
+    allStructure = args.a
+    allAnnotations = args.aa
+
+    if input_Path.endswith(".cif") or input_Path.endswith(".pdb"):
+        entry_id = os.fsdecode(input_Path)
+        generate_output_files(entry_id, input_Path, output_FolderPath, category, opt, allStructure, allAnnotations, mn)
+    elif os.path.isdir(input_Path):
+        # If it's a folder, execute for every .cif (and .pdb?)
+        directory = os.fsencode(input_Path)
+    
+        for file in os.listdir(directory):
+            filename = os.fsdecode(file)
+            # input just works with .cif and .pdb
+            if filename.endswith(".cif") or filename.endswith(".pdb"): 
+                entry_id = filename
+                generate_output_files(entry_id, filename, output_FolderPath, category, opt, allStructure, allAnnotations, mn)
---
+++ RNASecodaryStructure.py
@@ -0,0 +1,296 @@
+import os
+from PIL import Image, ImageDraw, ImageFont
+
+class RNASecondaryStructure:
+    def __init__(self, sequence, pdbid):
+        self.sequence = sequence
+        self.pdbid = pdbid
+        self.bonds = []
+
+    def add_bond(self, base1_number, base2_number, bond_type, model_number):
+        base1_number = int(base1_number)
+        base2_number = int(base2_number)
+
+        if base1_number == base2_number:
+            # Indici uguali, return 
+            return
+
+        if base1_number > base2_number:
+            # Scambia base1_number e base2_number (base1 indice sempre minore)
+            base1_number, base2_number = base2_number, base1_number
+
+        base_indexes = [int(base[0]) for base in self.sequence]
+        if base1_number not in base_indexes or base2_number not in base_indexes:
+            # Indice fuori dalla sequenza, return
+            return
+
+        for bond in self.bonds:
+            if bond.base1 == base1_number and bond.base2 == base2_number and bond.model_number == model_number:
+                # Il legame esiste già
+                return
+
+        # Tutti i controlli passati, aggiungi il legame
+        bond = Bond([base1_number, base2_number], bond_type, abs(model_number))
+        self.bonds.append(bond)
+
+    def write_output_file(self, bond_types, model_number, output_formats, outputFolder):
+        if 'bpseq' in output_formats:
+            self.write_bpseq_txt(bond_types, model_number, outputFolder)
+        if "aas" in output_formats:
+            self.write_aas_txt(bond_types, model_number, outputFolder)
+        if "tkz" in output_formats:
+            self.write_tkz_txt(bond_types, model_number, outputFolder)
+        if  "png" in output_formats:
+            self.write_arc_diagram_png(bond_types, model_number, outputFolder)
+
+    def generate_bpseq_sequence(self, bondType, model_number):
+        bpseq_sequence = []
+        for baseNumber, base in self.sequence:
+
+            baseNum1 = baseNumber
+            baseLetter = base
+            baseNum2 = 0
+
+            for bond in self.bonds:
+                if bond.bondType == bondType and int(baseNumber) == bond.base1 and abs(model_number) == bond.model_number:
+                    baseNum2 = bond.base2   
+                elif bond.bondType == bondType and int(baseNumber) == bond.base2 and abs(model_number) == bond.model_number:
+                    baseNum2 = bond.base1
+
+            bpseq_sequence.append(f"{baseNum1} {baseLetter} {baseNum2}")
+
+        return bpseq_sequence
+        
+    def generate_aas_sequence(self, bond_types, model_number):
+        aas_sequence = []
+        firstBaseIndex = int(self.sequence[0][0]) - 1
+        aas_sequence.append(''.join(pair[1] for pair in self.sequence))
+        
+        unique_bonds = set()
+        sorted_bonds = sorted(self.bonds, key=lambda bond: bond.base1)
+        
+        for bond in sorted_bonds:
+            if bond.bondType in bond_types and abs(model_number) == bond.model_number:
+                # Create a tuple representing the bond
+                bond_tuple = (bond.base1, bond.base2)
+                
+                # Check if the bond is already in the set
+                if bond_tuple in unique_bonds:
+                    continue  # Skip duplicate bond
+                
+                unique_bonds.add(bond_tuple)  # Add the bond to the set
+                aas_sequence.append(f"({bond.base1 - firstBaseIndex},{bond.base2 - firstBaseIndex});")
+        
+        return aas_sequence
+
+    def write_aas_txt(self, bond_types, model_number, output_folder):
+
+        if not self.any_bonds_found(bond_types, model_number):
+            return
+
+        filename = generate_file_name(self.pdbid, model_number, bond_types, "aas", output_folder, "txt")
+
+        aas = self.generate_aas_sequence(bond_types, model_number)
+
+        with open(filename,'w') as f:
+            f.write(str(aas[0] + "\n\n"))
+            aas.remove(aas[0])
+            for line in aas:
+                f.write(str(line))
+
+    def write_bpseq_txt(self, bond_type, model_number, output_folder):
+        if len(bond_type) > 1 or not self.any_bonds_found(bond_type, model_number): # Bpseq con più di un legame può causare conflitti
+            return
+
+        filename = generate_file_name(self.pdbid, model_number, bond_type, "bpseq", output_folder, "txt")
+        
+        bpseq = self.generate_bpseq_sequence(bond_type[0], model_number)
+        if bpseq == None:
+            return
+
+        with open(filename,'w') as f:
+            for line in bpseq:
+                f.write(str(line + "\n"))
+
+    def write_arc_diagram_png(self, bond_types, model_number, output_folder):
+
+        if not self.any_bonds_found(bond_types, model_number):
+            return
+
+        n = len(self.sequence)
+        start_number = int(self.sequence[0][0])
+        connections = []
+        filename = generate_file_name(self.pdbid, model_number, bond_types, "arc", output_folder, "png")
+
+        for bond in self.bonds:
+            if bond.bondType in bond_types and abs(model_number) == abs(bond.model_number):
+                connections.append([int(bond.base1), int(bond.base2)])
+
+        width = 120 + (n - 1) * 60
+        height = 120 + (n - 1) * 60 
+
+        # Crea una nuova immagine
+        image = Image.new("RGB", (width, height), "white")
+        draw = ImageDraw.Draw(image)
+
+        y = height // 2
+        start_x = 60
+        end_x = width - 60
+
+        # Linea orizzontale
+        line_color = (0, 0, 0) 
+        draw.line([(start_x, y), (end_x, y)], fill=line_color, width=2)
+
+        # Calcola la distanza fra i cerchi
+        gap = (end_x - start_x) / (n - 1)
+
+        circle_radius = 10
+        circle_color = (0, 0, 0)  # RGB nero
+        text_color = (0, 0, 0)  # RGB nero
+
+        # font e dimensioni del testo
+        font = ImageFont.truetype("arial.ttf", circle_radius*2)
+
+        for i in range(n):
+            center_x = start_x + i * gap
+            center = (int(center_x), y)
+
+            # Disegna i cerchi per le basi
+            draw.ellipse([(center[0] - circle_radius, center[1] - circle_radius),
+                        (center[0] + circle_radius, center[1] + circle_radius)],
+                        fill=circle_color)
+
+            # Scrivi i numeri
+            number = start_number + i
+            text = str(number)
+            text_bbox = draw.textbbox((0, 0), text, font=font)
+            text_width = text_bbox[2] - text_bbox[0]
+            text_height = text_bbox[3] - text_bbox[1]
+            text_x = center[0] - text_width // 2
+            text_y = center[1] - text_height // 2 + (gap * 2)
+            draw.text((text_x, text_y), text, font=font, fill=text_color)
+
+            # Scrivi le lettere
+            char = self.sequence[i][1]
+            char_bbox = draw.textbbox((0, 0), char, font=font)
+            char_width = char_bbox[2] - char_bbox[0]
+            char_x = center[0] - char_width // 2
+            char_y = center[1] + gap  # 5-pixel spacing
+            draw.text((char_x, char_y), char, font=font, fill=text_color)
+
+        # Archi
+        arc_color = (0, 0, 0)  # RGB colore blu
+        for connection in connections:
+            b1, b2 = connection
+            
+            start_index = b1 - start_number
+            end_index = b2 - start_number
+            start_x1 = start_x + (start_index * gap)
+            end_x = start_x + (end_index * gap)
+
+            # Calcola il centro del semicerchio
+            arc_center_x = (start_x1 + end_x) / 2
+            arc_center_y = y + circle_radius
+
+            # Calcola il raggio del cerchio
+            arc_radius = (end_x - start_x1) / 2
+
+            # Disegna il semicerchio
+            start_angle = 180
+            end_angle = 0
+            draw.arc([(arc_center_x - arc_radius, arc_center_y - arc_radius),
+                        (arc_center_x + arc_radius, arc_center_y + arc_radius)],
+                        start=start_angle, end=end_angle, fill=arc_color)
+
+        image.save(filename, "PNG")
+    
+    def write_tkz_txt(self, bond_types, model_number, output_folder):
+
+        if not self.any_bonds_found(bond_types, model_number):
+            return
+
+        filename = generate_file_name(self.pdbid, model_number, bond_types, "tkz", output_folder, "txt")
+
+        res = f"\\begin{{tikzpicture}}\n\t\\begin{{pgfonlayer}}{{nodelayer}}\n"
+
+        i = 0
+        si = self.sequence[0][0]
+        li = self.sequence[-1][0]
+        for n1,b in self.sequence:
+            res += f"\t\t\\node [draw, fill=black, circle] ({n1}) at ({i}, 0) " + "{}" + ";\n"
+            res += f"\t\t\\node [style=none] ({n1}a) at ({i}, -1) {{{b}}};\n"
+            res += f"\t\t\\node [style=none] ({n1}b) at ({i}, -2) {{{n1}}};\n"
+            i = i+1
+
+        res += f"\t\end{{pgfonlayer}}\t\n\\begin{{pgfonlayer}}{{edgelayer}}\n\t\t\draw ({si}.center) to ({li}.center);\n"
+
+        for n1,b in self.sequence:
+            for bond in self.bonds:
+                if bond.bondType in bond_types and n1 == bond.base1 and abs(model_number) == bond.model_number:
+                    res += f"\t\t\draw [bend left=90, looseness=2.00] ({n1}.center) to ({bond.base2}.center);\n"
+
+        res += f"\t\end{{pgfonlayer}}\n\end{{tikzpicture}}"
+
+        with open(filename,'w') as f:
+            f.write(res)
+
+    def any_bonds_found(self, bond_types, model_number):
+        for bond in self.bonds:
+            if bond.model_number == abs(model_number) and bond.bondType in bond_types:
+                return True
+
+def generate_file_name(pbdID, model_number, bond_types, output_format, output_folder_path, extension):
+
+    filename = ""
+
+    if model_number == -1:
+        if len(bond_types) == 1:
+            filename = os.path.join(output_folder_path, pbdID + "_" + bond_types[0] + "_" + output_format + "." + extension)
+        else:
+            filename = os.path.join(output_folder_path, pbdID  + "_" + output_format + "." + extension)
+    else:
+        if len(bond_types) == 1:
+            filename = os.path.join(output_folder_path, pbdID + "_" + bond_types[0] + "_" + output_format + "_" + str(model_number) + "." + extension)
+        else:
+            filename = os.path.join(output_folder_path, pbdID+ "_" + output_format + "_" + str(model_number) + "." + extension)
+
+    return filename
+
+class Bond:
+    def __init__(self, bases, bond_type, model_number):
+        self.bases = bases
+        self.bond_type = bond_type
+        self.model_number = model_number
+
+    @property
+    def base1(self):
+        return self.bases[0]
+
+    @property
+    def base2(self):
+        return self.bases[1]
+    
+    @property
+    def bondType(self):
+        return self.bond_type
+    
+    @property
+    def model_number(self):
+        return self._model_number
+
+    @model_number.setter
+    def model_number(self, value):
+        self._model_number = value
+
+#DEBUG
+# if __name__=="__main__":
+#     rna_structure = RNASecondaryStructure([[1, 'A'], [2, 'C'], [3, 'G'], [4, 'U'], [5, 'A'], [6, 'A']], "100d")
+#     rna_structure.add_bond(1, 3, 'cWW', 1)
+#     rna_structure.add_bond(2, 4, 'cWW', 1)
+#     rna_structure.add_bond(2, 6, 'cWW', 1)
+#     rna_structure.add_bond(3, 4, 'tHS', 3)
+#     rna_structure.add_bond(1, 6, 'tHS', 3)
+
+#     bpseq_sequence = rna_structure.generate_bpseq_sequence("cWW", 1)
+#     outputs = ["aas","bpseq","tkz", "png"]
+#     rna_structure.write_output_file(["cWW"], 3, outputs, "C:\\Users\\lolli\\OneDrive\\Desktop\\NAPairwiseInteractions\\")
